5420695ce70b07d0182d2d5130eadc1dc96c3dd5
Tanner Mares <tannermares@gmail.com>
2013-03-13 00:22:51 -0700

    event object is passed to anonymous function. You can use 'this' inside the function to refer to itself

--- jquery.js
+++ jquery.js
@@ -1,6 +1,6 @@
 // Updated Method since JQuery 1.7
 element_name = container + " .plan";
 
-$(element_name).off().on('click', function(el) { 
-  $(el).hide();
+$(element_name).off().on('click', function(event) { 
+  $(this).hide();
 });
