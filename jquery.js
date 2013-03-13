dedd0ebbab5c2aca9a3a8d45d56000c68302a9e5
Tanner Mares <tannermares@gmail.com>
2013-03-13 00:27:49 -0700

    added check to prevent default element action from firing

--- jquery.js
+++ jquery.js
@@ -1,6 +1,8 @@
 // Updated Method since JQuery 1.7
 element_name = container + " .plan";
-
+// Replaced die() and live() methods with new off() and on().
+// added event.preventDefault() to stop default action from clicking element
 $(element_name).off().on('click', function(event) { 
+  event.preventDefault();
   $(this).hide();
 });
